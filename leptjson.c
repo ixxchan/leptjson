9542a5ca7f3d8906ecdff3870fda330fe145abf6
xxchan <xxchan@sjtu.edu.cn>
2020-01-28 18:40:12 +0800

    add basic API and unit test

--- leptjson.c
+++ leptjson.c
@@ -2,6 +2,11 @@
 
 #include <stdio.h>
 
-void hello(void) {
-    printf("Hello, World!\n");
+int lept_parse(lept_value *v, const char *json) {
+    return 0;
 }
+
+lept_type lept_get_type(const lept_value *v) {
+    return LEPT_OBJECT;
+}
+
